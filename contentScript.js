a0da013c96e6116388b70c757f30fc0b97253132
syaifulhuseinnn <syaifulhuseinnn@gmail.com>
2020-12-12 16:13:35 +0700

    DOM manipulate. DONE!

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,26 @@
-document.addEventListener("DOMNodeInserted", function(event) {
-    if(document.querySelector(".euiBreadcrumb.euiBreadcrumb--last" != null)) {
-        let title = document.querySelector(".euiBreadcrumb.euiBreadcrumb--last")
-        title.innerHTML = "SAMPLE TITLE"
+function checkClass() {
+    let div = document.querySelector('div[style="width: 400px;"]')
+    let euiPanel = document.querySelector('div[data-test-subj="comboBoxOptionsList filterFieldSuggestionList-optionsList"]')
+    let reactVirtualizedGrid = document.querySelector('.ReactVirtualized__Grid.ReactVirtualized__List')
+    let reactVirtualizedInnerScrollContainer = document.querySelector('.ReactVirtualized__Grid__innerScrollContainer')
+    let euiComboxGlobalFilterEditor = document.querySelector('.euiComboBox.globalFilterEditor__fieldInput')
+    let euiContextMenu = document.querySelector('div.euiContextMenu[style="height: 297px; width: 420px;"]')
+
+    if (div != null) {
+        div.style.width = "620px"
+        euiComboxGlobalFilterEditor.style.maxWidth = "408px"
     }
-})
+
+    if (euiPanel != null) {
+        euiPanel.style.width = "408px"
+        reactVirtualizedGrid.style.width = "408px"
+        reactVirtualizedInnerScrollContainer.style.maxWidth = "408px"
+    }
+
+    if (euiContextMenu != null) {
+        euiContextMenu.style.width = "620px"
+        euiComboxGlobalFilterEditor.style.maxWidth = "408px"
+    }
+}
+
+let scheduller = setInterval(checkClass, 50)
