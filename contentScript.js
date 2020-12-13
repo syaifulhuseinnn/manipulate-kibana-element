a15e9ceb734f00e904eb1e62f551876b6fc2960c
syaifulhuseinnn <syaifulhuseinnn@gmail.com>
2020-12-13 14:22:00 +0700

    extension landing page

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,6 @@
 function checkClass() {
     let div = document.querySelector('div[style="width: 400px;"]')
-    let euiPanel = document.querySelector('div[data-test-subj="comboBoxOptionsList filterFieldSuggestionList-optionsList"]')
+    let euiPanel = document.querySelector('div.euiPanel.euiComboBoxOptionsList.euiComboBoxOptionsList--bottom[data-test-subj="comboBoxOptionsList filterFieldSuggestionList-optionsList"]')
     let reactVirtualizedGrid = document.querySelector('.ReactVirtualized__Grid.ReactVirtualized__List')
     let reactVirtualizedInnerScrollContainer = document.querySelector('.ReactVirtualized__Grid__innerScrollContainer')
     let euiComboxGlobalFilterEditor = document.querySelector('.euiComboBox.globalFilterEditor__fieldInput')
@@ -13,7 +13,6 @@ function checkClass() {
 
     if (euiPanel != null) {
         euiPanel.style.width = "408px"
-        reactVirtualizedGrid.style.width = "408px"
         reactVirtualizedInnerScrollContainer.style.maxWidth = "408px"
     }
 
